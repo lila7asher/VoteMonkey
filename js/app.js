b0f5336711bdaf9e80fc9d3439f91ae4c55c99ce
Lila Asher <lila7asher@gmail.com>
2020-04-23 14:26:48 -0400

    completes JS function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 const submitBtn = document.querySelector("#submitbtn");
 const survey = document.querySelector("#issuebox");
 const result = document.querySelector("#suggestion");
-const name = document.querySelector("#candname");
+const candname = document.querySelector("#candname");
 const suggest = document.querySelector("#suggestlink");
 
 submitBtn.addEventListener("click", ()=>{
-    survey.style.display = "none";
-    result.style.display = "block";
+    if(document.querySelector("#banana").checked){
+        candname.textContent = "Chaussette";
+        suggest.href = "profiles.html#chaussette";
+        survey.style.display = "none";
+        result.style.display = "block";
+    }else if(document.querySelector("#air").checked){
+        candname.textContent = "Swinger";
+        suggest.href = "profiles.html#swinger";
+        survey.style.display = "none";
+        result.style.display = "block";
+    }else if(document.querySelector("#humans").checked){
+        candname.textContent = "Napoleon";
+        suggest.href = "profiles.html#napoleon";
+        survey.style.display = "none";
+        result.style.display = "block";
+    }else{
+        alert("Please select an issue from the list")
+    }
 })
-
-console.log("done")
